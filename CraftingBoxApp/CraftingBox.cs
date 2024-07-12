bcac42b61af351a89c634367c151146682efc913
Chnapak <matej.hladecek@gmail.com>
2024-07-12 21:54:43 +0200

    Created repository and added comments.

---
+++ CraftingBoxApp/CraftingBox.cs
@@ -0,0 +1,41 @@
+﻿namespace CraftingBoxApp
+{
+    internal class CraftingBox
+    {
+        // This class keeps track of wood suply and it's inventory.
+        private int remainingWood = 0;
+        private string[] items = new string[3] {"prkno", "židle", "stůl"};
+        private int[] count = new int[3];
+        public string CraftMeSomething(int woodAmount)
+        {
+            
+            if (woodAmount + remainingWood >= 7)
+            {
+                remainingWood = woodAmount + remainingWood - 7;
+                count[2]++;
+                return items[2];
+            }
+            else if (woodAmount >= 5)
+            {
+                remainingWood = woodAmount + remainingWood - 5;
+                count[1]++;
+                return items[1];
+            }
+            else if (woodAmount >= 3) 
+            {
+                remainingWood = woodAmount + remainingWood - 3;
+                count[0]++;
+                return items[0];
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+
+        public int[] HowMuchCreated()
+        {
+            return count;
+        }
+    }
+}
---
+++ CraftingBoxApp/RunApp.cs
@@ -0,0 +1,36 @@
+﻿namespace CraftingBoxApp
+{
+    internal class RunApp
+    {
+        public void Run()
+        {
+            // Task was to create a crafting table of sorts, who gets a random supply of wood.
+            // It instantly crafts something with the wood it has remaining and with the ones he got.
+
+            var rand_woodAmount = new Random();
+
+            var craftingTable = new CraftingBox();
+
+
+            for (int i = 0; i < 10; i++) 
+            {
+                // Translation: You have crafted: (item)
+                Console.WriteLine("Vytvořil jsi: " + craftingTable.CraftMeSomething(rand_woodAmount.Next(0,11)));
+            }
+
+            int[] amountOfStuff = craftingTable.HowMuchCreated();
+
+            // --- Translation ---
+            // Amount of crafted items:
+            // Plank:
+            // Chair:
+            // Table:
+            // Total:
+            Console.WriteLine("Počet vytvořených předmětů: ");
+            Console.WriteLine("Prkna: " + amountOfStuff[0]);
+            Console.WriteLine("Židle: " + amountOfStuff[1]);
+            Console.WriteLine("Stůl: " + amountOfStuff[2]);
+            Console.WriteLine("Celkem: " + (amountOfStuff[0] + amountOfStuff[1] + amountOfStuff[2]));
+        }
+    }
+}
